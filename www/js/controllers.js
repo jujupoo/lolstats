e1b6d12fa211cc7c689a7f4b09a75bd4a09be7e4
jujupoo <ellisju4@msu.edu>
2016-10-30 21:45:06 -0400

    lolstats factory controller

---
+++ www/js/controllers.js
@@ -0,0 +1,45 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function ($scope) { })
+
+.controller('LOLstatsCtrl', function ($scope,$stateParams,LOLstats) {
+    var $scope.stats = null;
+    var initStats = function($stateParams.summonerID){
+
+    
+        LOLstats.getStats(summonerID).then(
+            function(result){
+                $scope.stats = result.data;
+            },
+                function (error) {
+                    UtilService.showError(error);
+                }
+        );
+    }
+})
+
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
